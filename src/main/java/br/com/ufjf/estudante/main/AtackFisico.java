2d7ce2e471db5ba17ec0bf77146199fc1935f9b1
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2021-02-18 10:39:45 -0300

    Corrigido path de imagem pra linux e adicionado nome no inicio de todos arquivos

--- src/main/java/br/com/ufjf/estudante/main/AtackFisico.java
+++ src/main/java/br/com/ufjf/estudante/main/AtackFisico.java
@@ -1,8 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * Gabriel Pires Ferreira 201935023
+ * Matheus do Nascimento Pereira da Costa 201676003
+ * Luiz Henrique de Oliveira Pereira 201635009
+ * Paulo Victor de Magalhaes Rozatto 201935027
+*/
 package br.com.ufjf.estudante.main;
 
  /**
--- src/main/java/br/com/ufjf/estudante/main/Personagem.java
+++ src/main/java/br/com/ufjf/estudante/main/Personagem.java
@@ -1,7 +1,11 @@
-
+/*
+ * Gabriel Pires Ferreira 201935023
+ * Matheus do Nascimento Pereira da Costa 201676003
+ * Luiz Henrique de Oliveira Pereira 201635009
+ * Paulo Victor de Magalhaes Rozatto 201935027
+*/
 package br.com.ufjf.estudante.main;
 
-import java.awt.Image;
 import javax.swing.ImageIcon;
 
 
@@ -308,16 +312,16 @@ public class Personagem {
         switch (classe) {
 
             case 1:
-                sprite = new ImageIcon("res\\jogador\\guerreiro.png"); //Pasta onde esta a imagem
+                sprite = new ImageIcon("res/jogador/guerreiro.png"); //Pasta onde esta a imagem
                 break;
             case 2:
-                sprite = new ImageIcon("res\\jogador\\mago.png"); //Pasta onde esta a imagem
+                sprite = new ImageIcon("res/jogador/mago.png"); //Pasta onde esta a imagem
                 break;
             case 3:
-                sprite = new ImageIcon("res\\jogador\\ladino.png"); //Pasta onde esta a imagem
+                sprite = new ImageIcon("res/jogador/ladino.png"); //Pasta onde esta a imagem
                 break;
             default:
-                sprite = new ImageIcon("res\\jogador\\guerreiro.png"); //Pasta onde esta a imagem
+                sprite = new ImageIcon("res/jogador/guerreiro.png"); //Pasta onde esta a imagem
         }
     }
         
